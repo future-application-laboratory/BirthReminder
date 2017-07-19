16c5ae7e9f6c8c8d8cce6e38247ab01b9bdd3e3e
CaptainYukinoshitaHachiman <CaptainYukinoshitaHachiman@protonmail.com>
2017-07-19 09:19:43 +0800

    Initial Commit

---
+++ BirthdayReminder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BirthdayReminder
+//
+//  Created by Jacky Yu on 19/07/2017.
+//  Copyright © 2017 CaptainYukinoshitaHachiman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
