6301a4e2171c4a3393d6dde98a291cd6dd448761
CaptainYukinoshitaHachiman <CaptainYukinoshitaHachiman@protonmail.com>
2017-08-25 11:35:21 +0800

    add additional acknowledgments

--- BirthdayReminder/Controllers/SettingsViewController.swift
+++ BirthdayReminder/Controllers/SettingsViewController.swift
@@ -21,6 +21,10 @@ class SettingsViewController: UITableViewController {
         if indexPath.section == 1 {
             let path = Bundle.main.path(forResource: "Pods-BirthdayReminder-acknowledgements", ofType: "plist")
             let controller = AcknowListViewController(acknowledgementsPlistPath: path)
+            controller.acknowledgements! += [
+                Acknow(title: "Material Icons", text: "Icons in the app are from Google Materail Icons.\nThe icons are available under the Apache License Version 2.0. We'd love attribution in your app's \"about\" screen, but it's not required. The only thing we ask is that you not re-sell these icons. https://material.io/icons/"),
+                Acknow(title: "Pics on the Server", text: "All the pics on the server are collected from the Internet, if you own the copyright/copyleft and don't want to see it here, please contact me at CaptainYukinoshitaHachiman@protonmail.com")
+            ]
             
             controller.tableView.backgroundColor = UIColor.flatGreen
             controller.tableView.visibleCells.forEach { cell in
